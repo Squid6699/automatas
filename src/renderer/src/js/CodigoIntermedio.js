dae25365292540d3fc6ec6f21916edbcc7f5ea63
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-10-03 12:45:43 -0700

    CodigoIntermedio

--- src/renderer/src/js/CodigoIntermedio.js
+++ src/renderer/src/js/CodigoIntermedio.js
@@ -4,27 +4,27 @@ export function obtenerCodigoIntermedio(){
     var data = "\t\t .DATA \n\n";
 
     variables.forEach((contenido, id) => {
-        // var numero = parseInt(contenido);
+        var numero = parseInt(contenido);
         
         if (contenido === "" || isNaN(contenido)){
             data = data + id.toUpperCase() + "\t" + "DW" + "\t" + "?" + "\n"
             return;
         }
 
-        // if (numero >= 0 && numero <= 255 || numero >= -127 && numero <= 127){
-        //     data = data + id.toUpperCase() + "\t" + "DB" + "\t" + contenido + "\n"
-        //     return;
-        // }
+        if (numero >= 0 && numero <= 255 || numero >= -127 && numero <= 127){
+            data = data + id.toUpperCase() + "\t" + "DB" + "\t" + contenido + "\n"
+            return;
+        }
 
-        // if (numero >= 0 && numero <= 65535 || numero >= -32768 && numero <= 32767){
-        //     data = data + id.toUpperCase() + "\t" + "DW" + "\t" + contenido + "\n"
-        //     return;
-        // }
+        if (numero >= 0 && numero <= 65535 || numero >= -32768 && numero <= 32767){
+            data = data + id.toUpperCase() + "\t" + "DW" + "\t" + contenido + "\n"
+            return;
+        }
 
-        // if (numero >= 0 && numero <= 4294967295 || numero >= -2147483648 && numero <= 2147483647){
-        //     data = data + id.toUpperCase() + "\t" + "DD" + "\t" + contenido + "\n"
-        //     return;
-        // }
+        if (numero >= 0 && numero <= 4294967295 || numero >= -2147483648 && numero <= 2147483647){
+            data = data + id.toUpperCase() + "\t" + "DD" + "\t" + contenido + "\n"
+            return;
+        }
 
     });
     console.log(data);
